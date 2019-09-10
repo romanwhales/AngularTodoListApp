6d1db7196a300c314e42c0e07d86f5e5d6899ebe
Sunmonu-Adedeji Olawale <ssg315ass@yahoo.com>
2019-09-10 04:06:59 +0100

    First commit

---
+++ src/app/model.ts
@@ -0,0 +1,32 @@
+export class Model{
+    user;
+    items;
+    constructor(){
+        this.user = "Wale";
+        this.items = [
+            new TodoItem("Buy Flowers",false),
+            new TodoItem("Get Shoes",false),
+            new TodoItem("Collect Tickets",false),
+            new TodoItem("Call Joe",false)
+        ]
+    }
+}
+
+export class TodoItem{
+    action;
+    done;
+
+    constructor(action,done){
+        this.action = action;
+        this.done = done;
+    }
+}
+
+
+// var model = {
+//     user: "Adam",
+//     items: [{ action: "Buy Flowers", done: false },
+//     { action: "Get Shoes", done: false },
+//     { action: "Collect Tickets", done: true },
+//     { action: "Call Joe", done: false }]
+// };
